977a7f6361af25eb5ebe36bde6ace668833602aa
unknown <769540542@qq.com>
2024-04-26 09:42:47 +0800

    123

--- project/Myproject/Myproject.cpp
+++ project/Myproject/Myproject.cpp
@@ -18,50 +18,52 @@ int main()
 
     //040Opencv之C++进阶内容(2)_3、C++的函数模板详解-2
     {
-        /*  TemplateUse tu;
-          tu.Show();*/
-    }
+        {
+            /*  TemplateUse tu;
+              tu.Show();*/
+        }
 
 
-    {
-        /*TemplateMatching tm;
-        tm.Show();*/
-    }
-
-    /* 
-    * 注意
-    * 在我们殡仪的时候，编译器内部会自动生成这些模板函数，
-    * 然后再进行调用，当然，这些都是编译器内部生成的，我们
-    * 可以不用去了解太深，我们指定这个概念就好了。
-    */
-
-    // 函数模板多类型定义
-
-    // 案例1
-    {
-        /*FunctionTemplateMultiTypeDefinition ftmtd;
-        ftmtd.Show();*/
+        {
+            /*TemplateMatching tm;
+            tm.Show();*/
+        }
 
         /*
-        * 案例1编译肯定报错的，因为定义了T2 没有使用导致T2没有办法推断
+        * 注意
+        * 在我们殡仪的时候，编译器内部会自动生成这些模板函数，
+        * 然后再进行调用，当然，这些都是编译器内部生成的，我们
+        * 可以不用去了解太深，我们指定这个概念就好了。
         */
-    }
 
-    //案例2
-    {
-       /* FunctionTemplateMultiTypeDefinitionCase2  ftmtdc2;
-        ftmtdc2.Show();*/
-        /*
-       * Max<int,int>(a,b); // 显式类型调用
-       * Max(a,b); // 自动数据类型推导
-       */
-    }
+        // 函数模板多类型定义
 
-    //综合案例  FunctionTemplateMultiTypeDefinitionComprehensiveCase
-    // son
-    {
-        FunctionTemplateMultiTypeDefinitionComprehensiveCase ftmtdcc;
-        ftmtdcc.Show();
+        // 案例1
+        {
+            /*FunctionTemplateMultiTypeDefinition ftmtd;
+            ftmtd.Show();*/
+
+            /*
+            * 案例1编译肯定报错的，因为定义了T2 没有使用导致T2没有办法推断
+            */
+        }
+
+        //案例2
+        {
+            /* FunctionTemplateMultiTypeDefinitionCase2  ftmtdc2;
+             ftmtdc2.Show();*/
+             /*
+            * Max<int,int>(a,b); // 显式类型调用
+            * Max(a,b); // 自动数据类型推导
+            */
+        }
+
+        //综合案例  FunctionTemplateMultiTypeDefinitionComprehensiveCase
+        // son
+        {
+            FunctionTemplateMultiTypeDefinitionComprehensiveCase ftmtdcc;
+            ftmtdcc.Show();
+        }
     }
 
 #pragma endregion
